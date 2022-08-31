f063a833a57a0b983bef35faa1f142950d7a64bb
Kostadin Madjerski <dinka4aps@gmail.com>
2022-08-31 15:29:55 +0300

    less code

--- softuni/Python Advanced/Lists as Stacks and Queues/Excersise/key_revolver.py
+++ softuni/Python Advanced/Lists as Stacks and Queues/Excersise/key_revolver.py
@@ -12,23 +12,19 @@ barrel = size_of_a_gun_barrel
 broken_locks = False
 
 while len(bullets) != 0: 
+    bullet_shot = bullets.pop()
+    barrel -=1
+    value_of_the_inteligence -= price_for_a_bullet
+    
+    if int(bullet_shot) <= int(lock_shot):
+        print(f'Bang!')
+        lock_shot = locks.popleft()
+    
+    else:
+        print('Ping!')
     if barrel == 0:
         print(f'Reloading!')
         barrel = size_of_a_gun_barrel
-    bullet_shot = bullets.pop()
-    lock_shot = locks.popleft()
-    if int(bullet_shot) > int(lock_shot):
-        print('Ping!')
-        barrel -=1
-        locks.appendleft(lock_shot)
-        value_of_the_inteligence -= price_for_a_bullet
-    elif int(bullet_shot) <= int(lock_shot):
-        print(f'Bang!')
-        barrel -=1
-        value_of_the_inteligence -= price_for_a_bullet
-    if len(locks) <= 0:
-        broken_locks = True
-        break
 
 if broken_locks:
     print(f'{len(bullets)} bullets left. Earned ${value_of_the_inteligence}$')
