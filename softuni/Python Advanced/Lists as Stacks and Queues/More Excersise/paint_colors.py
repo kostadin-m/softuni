820dea42c0ca25289b1cf275268b53dd555dd6d7
Kostadin Madjerski <dinka4aps@gmail.com>
2022-09-02 20:24:31 +0300

    ref

---
+++ softuni/Python Advanced/Lists as Stacks and Queues/More Excersise/paint_colors.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+
+main_colors = ['red','yellow','blue']
+secondary_color = ["orange", "purple", "green"]
+
+needed_for_secondary = {
+    'orange' : ['red', 'yellow'],
+    'purple' : ['red', 'blue'],
+    'green' : ['yellow', 'blue'],
+}
+
+found_colors = []
+
+line = deque(input().split())
+
+
+while line:
+
+    first = line.popleft()
+    second = line.pop() if line else ''
+    result  = first+second 
+    if result in main_colors or result in secondary_color:
+        found_colors.append(result)
+        continue
+    result = second + first
+    if result in main_colors or result in secondary_color:
+        found_colors.append(result)
+        continue
+       
+    first = first[:-1]
+    second = second[:-1]
+    if first:
+
+        line.insert(int(len(line) // 2), first)
+    if second:
+        line.insert(int(len(line) // 2), second)
+
+result = []
+
+
+for color in found_colors:
+    if color in main_colors:
+        result.append(color)
+
+    else:
+        is_valid = True
+        for required in needed_for_secondary[color]:
+            if required not in found_colors:
+                is_valid = False
+                break
+        if is_valid:
+            result.append(color)
+
+
+print(result)
