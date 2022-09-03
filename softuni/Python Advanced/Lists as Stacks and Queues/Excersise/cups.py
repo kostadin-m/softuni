ab219f095e0c655d54ce34b35d31c4b16bb2b790
Kostadin Madjerski <dinka4aps@gmail.com>
2022-09-03 15:21:25 +0300

    =changes

--- softuni/Python Advanced/Lists as Stacks and Queues/Excersise/cups.py
+++ softuni/Python Advanced/Lists as Stacks and Queues/Excersise/cups.py
@@ -5,9 +5,7 @@ bottles_stack = [int(x) for x in input().split()]
 wasted_water = 0
 
 
-while cups:
-    if  not bottles_stack:
-        break
+while cups and bottles_stack:
     cup = cups.popleft()
     bottle = bottles_stack.pop()
     if cup > bottle:
@@ -15,8 +13,8 @@ while cups:
         cups.appendleft(cup)
     else:
         wasted_water += abs(cup - bottle)
-if len(cups) == 0:
-    print('Bottles: ',' '.join([str(y) for y in bottles_stack]))
+if not cups:
+    print('Bottles:',' '.join([str(y) for y in bottles_stack]))
     print(f'Wasted litters of water: {wasted_water}')
 else:
     print('Cups:',' '.join([str(y) for y in cups]))
