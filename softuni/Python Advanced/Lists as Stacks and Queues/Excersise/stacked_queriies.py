5840136f63db940e1d923c35fc1896853c9e00ea
Kostadin Madjerski <dinka4aps@gmail.com>
2022-09-03 15:51:52 +0300

    = changes

--- softuni/Python Advanced/Lists as Stacks and Queues/Excersise/stacked_queriies.py
+++ softuni/Python Advanced/Lists as Stacks and Queues/Excersise/stacked_queriies.py
@@ -1,21 +1,36 @@
+from collections import deque
+
+
 n = int(input())
 stacked_numbers = []
-for _ in range(n):
-    line = input().split()
-    command = line[0]
-    if command =='1':
-        stacked_numbers.append(int(line[1]))
-    elif command == '2' and stacked_numbers:
-        stacked_numbers.pop()
-    elif command == '3'and stacked_numbers:
-        print(max(stacked_numbers))
-    elif command == '4' and stacked_numbers:
-        print(min(stacked_numbers))
-while stacked_numbers:
-    last = stacked_numbers.pop()
-    if stacked_numbers:
-        print(last,end=', ')
-    else:
-        print(last)
 
+def one():
+    stacked_numbers.append(int(text))
+def two():
+    if stacked_numbers:
+        stacked_numbers.pop()
+def three():
+    if stacked_numbers:
+        print(max(stacked_numbers))
+def four():
+    if stacked_numbers:
+        print(min(stacked_numbers))
+
+commands = {
+    1 : one,
+    2 : two,
+    3 : three,
+}
+
+
+
+for _ in range(n):
+    line = deque(input().split())
+    command = int(line.popleft())
+    text = int(line[0]) if line else ''
+    action = commands.get(command, four)
+    action()
+
+
+print(', '.join(str(x) for x in reversed(stacked_numbers)))
 
