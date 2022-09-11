c70df834f04d0819df5a879adb27a473d1a92bfe
Kostadin Madjerski <dinka4aps@gmail.com>
2022-09-11 15:50:38 +0300

    =ref

--- softuni/Random /tick_tack_toe_checker.py
+++ softuni/Random /tick_tack_toe_checker.py
@@ -1,10 +1,17 @@
-
 import time
 from collections import deque
 
+table = deque([
+    [0,0,0],
+    [0,0,0],
+    [0,0,0],])
+print(f'                         GAME BOARD          ')
+print(' ')
+for i in table:
+    print('                          ',' '.join(str(x) for x in i[::-1]))
+game = []
 
 def countdown(t):
-    
     while t:
         mins, secs = divmod(t, 60)
         timer = 'CHECKING RESULTS... {:02d}:{:02d}'.format(mins, secs)
@@ -19,31 +26,17 @@ def check_winner():
         if game[row][0] == game[row][1] == game[row][2]: # Checking rows to see if 1/0 is a winner
             winner = True
             break
-    for column in range(3): # If there is no winner in rows we check collumns
-        if winner:
-            break
-        else:
+    if not winner:
+        for column in range(3): # If there is no winner in rows we check collumns
             if game[0][column] == game[1][column] == game[2][column]:
                 winner = True
-    
+                break
     if not winner: #If there is no winner in collumns nor rows we check diagonals
         if game[0][0] == game[1][1] == game[2][2] or game[0][2] == game[1][1] == game[2][0]:
             winner = True
     result = game[0][0]
     return winner
 
-
-table = deque([
-    [0,0,0],
-    [0,0,0],
-    [0,0,0],
-])
-print(f'                         GAME BOARD          ')
-print(' ')
-for i in table:
-    print('                          ',' '.join(str(x) for x in i[::-1]))
-
-game = []
 
 for x in range(3):
     current_row = list(input().split(' '))
@@ -55,9 +48,7 @@ for i in table:
     print('                          ',' '.join(str(x) for x in i))
 
 check_winner()
-
 countdown(3)
-
 
 if check_winner:
     print('                       WINNER: ',result.upper())
