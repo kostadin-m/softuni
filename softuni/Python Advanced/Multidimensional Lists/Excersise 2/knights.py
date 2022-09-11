7449bcea291a28271f3e3806d51c82ba9f610a2a
Kostadin Madjerski <dinka4aps@gmail.com>
2022-09-11 17:18:51 +0300

    =ref

--- softuni/Python Advanced/Multidimensional Lists/Excersise 2/knights.py
+++ softuni/Python Advanced/Multidimensional Lists/Excersise 2/knights.py
@@ -1,16 +1,4 @@
-def is_valid(row,col,rows,cols):
-    return 0<= row and row< rows and 0<= col and col < cols
-
-def check_movement():
-    knight = 0
-    for movement_row,movement_col in movement.values():
-        next_row,next_col = knight_row + movement_row , knight_col + movement_col
-        if is_valid(next_row,next_col,rows,collumns) and matrix[next_row][next_col] =='K':
-            knight +=1
-    return knight
-
-rows= int(input())
-collumns = rows
+size= int(input())
 knight_position = set()
 matrix = []
 removed = 0
@@ -18,16 +6,30 @@ movement = {
     "up left": [-2, -1], "up right": [-2, 1], "down left": [2, -1], "down right": [2, 1],
     "left up": [-1, -2], "left down": [1, -2], "right up": [-1, 2], "right down": [1, 2],
 }
-for row in range(rows):
+
+def is_valid(row,col,size):
+    return 0<= row and row< size and 0<= col and col < size
+
+def check_movement():
+    knight = 0
+    for movement_row,movement_col in movement.values():
+        next_row,next_col = knight_row + movement_row , knight_col + movement_col
+        if is_valid(next_row,next_col,size) and matrix[next_row][next_col] =='K':
+            knight +=1
+    return knight
+
+
+
+for row in range(size):
     matrix.append([str(x) for x in list(input())])
-    for col in range(collumns):
+    for col in range(size):
         if matrix[row][col] == 'K':
             knight_position.add(f'{row} {col}')
 
 while True:
     value = {}
-    for row in range(rows):
-        for col in range(collumns):
+    for row in range(size):
+        for col in range(size):
             if matrix[row][col] == '0':
                 continue
             knight_row,knight_col = row,col
