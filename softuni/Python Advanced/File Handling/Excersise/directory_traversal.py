de182f1bbbf07332d1db8ca459db5d961b764b2e
Kostadin Madjerski <dinka4aps@gmail.com>
2022-09-24 14:04:08 +0300

    =ref

---
+++ softuni/Python Advanced/File Handling/Excersise/directory_traversal.py
@@ -0,0 +1,21 @@
+from os import walk 
+import os
+
+files_ext = {}
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+for _,_, files in walk(dir_path):
+    break
+for file in files:
+    file,ext = file.split('.')
+    if ext not in files_ext:
+        files_ext[ext] = []
+    files_ext[ext].append(file)
+
+
+
+
+for key,value in sorted(files_ext.items()):
+    print(f'.{key}')
+    print("\n".join([f'- - -{x}.{key}' for x in value]))
