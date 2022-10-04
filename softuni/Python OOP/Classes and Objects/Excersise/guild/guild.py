02ad6c07a4e95a9878dbac7b1468215110b90c17
Kostadin Madjerski <dinka4aps@gmail.com>
2022-10-04 22:21:27 +0300

    =ref

--- softuni/Python OOP/Classes and Objects/Excersise/guild/guild.py
+++ softuni/Python OOP/Classes and Objects/Excersise/guild/guild.py
@@ -6,21 +6,27 @@ class Guild:
     def assign_player(self, player_name):
         if player_name.guild != "Unaffiliated":
             return f"Player {player_name.name} is in another guild."
-        elif player_name not in self.players:
-            self.players.append(player_name)
-            player_name.guild = self.guild_name
-            return f'Welcome player {player_name.name} to the guild {self.guild_name}'
+        elif self.player_in_guild(player_name.name):
+            return f'Player {player_name.name} is already in the guild.'
 
-        return f'Player {player_name.name} is already in the guild.'
+        self.players.append(player_name)
+        player_name.guild = self.guild_name
+        return f'Welcome player {player_name.name} to the guild {self.guild_name}'
 
     def kick_player(self, player_name: str):
-        for x in self.players:
-            if x.name == player_name:
-                x.guild = "Unaffiliated"
-                self.players.remove(x)
-                return f"Player {player_name} has been removed from the guild."
+        player_in_guild = self.player_in_guild(player_name)
+        if not player_in_guild:
+            return f"Player {player_name} is not in the guild."
 
-        return f"Player {player_name} is not in the guild."
+        player_in_guild.guild = "Unaffiliated"
+        self.players.remove(player_in_guild)
+        return f"Player {player_name} has been removed from the guild."
 
     def guild_info(self):
         return f'Guild: {self.guild_name}\n' + '\n'.join([x.player_info() for x in self.players])
+
+    def player_in_guild(self, name):
+        for x in self.players:
+            if x.name == name:
+                return x
+        return False
