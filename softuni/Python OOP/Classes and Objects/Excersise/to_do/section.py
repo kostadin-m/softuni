187cad23c20511a8ff0f72de233204de62d6483c
Kostadin Madjerski <dinka4aps@gmail.com>
2022-10-05 09:26:11 +0300

    =ref

--- softuni/Python OOP/Classes and Objects/Excersise/to_do/section.py
+++ softuni/Python OOP/Classes and Objects/Excersise/to_do/section.py
@@ -4,17 +4,19 @@ class Section:
         self.tasks = []
 
     def add_task(self, new_task):
-        if not self.contains_task(new_task.name):
-            self.tasks.append(new_task)
-            return f"Task {new_task.details()} is added to the section"
-        return f"Task is already in the section {self.name}"
+        if self.contains_task(new_task.name):
+            return f"Task is already in the section {self.name}"
+
+        self.tasks.append(new_task)
+        return f"Task {new_task.details()} is added to the section"
 
     def complete_task(self, check_task):
         task_exist = self.contains_task(check_task)
-        if task_exist:
-            task_exist.completed = True
-            return f"Completed task {check_task}"
-        return f"Could not find task with the name {check_task}"
+        if not task_exist:
+            return f"Could not find task with the name {check_task}"
+
+        task_exist.completed = True
+        return f"Completed task {check_task}"
 
     def clean_section(self):
         counter = 0
