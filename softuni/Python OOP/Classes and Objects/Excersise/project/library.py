3e6387ec7c8d4f0771f97c1d4e1c36dcccad84cb
Kostadin Madjerski <dinka4aps@gmail.com>
2022-10-11 10:18:46 +0300

    =ref

--- softuni/Python OOP/Classes and Objects/Excersise/project/library.py
+++ softuni/Python OOP/Classes and Objects/Excersise/project/library.py
@@ -10,39 +10,39 @@ class Library:
     def get_book(self, author: str, book_name: str, days_to_return: int, user: User):
         if book_name not in self.books_available.get(author):
             return f'The book "{book_name}" is already rented and will be available in {self.rented_books[user.username][book_name]} days!'
-        self.add_to_user_books(user, book_name)
-        self.remove_from_available_books(author, book_name)
-        self.add_to_rented_books(user.username, book_name, days_to_return)
+        self.__add_to_user_books(user, book_name)
+        self.__remove_from_available_books(author, book_name)
+        self.__add_to_rented_books(user.username, book_name, days_to_return)
 
         return f"{book_name} successfully rented for the next {days_to_return} days!"
 
     def return_book(self, author: str, book_name: str, user: User):
         if book_name not in user.books:
             return f"{user.username} doesn't have this book in his/her records!"
-        self.remove_from_user_books(user, book_name)
-        self.add_to_available_books(author, book_name)
-        self.remove_from_rented_books(user.username, book_name)
+        self.__remove_from_user_books(user, book_name)
+        self.__add_to_available_books(author, book_name)
+        self.__remove_from_rented_books(user.username, book_name)
 
 
 
 
-    def add_to_available_books(self, author, book):
+    def __add_to_available_books(self, author, book):
         self.books_available[author].append(book)
 
-    def remove_from_available_books(self, author, book):
+    def __remove_from_available_books(self, author, book):
         self.books_available[author].remove(book)
 
-    def add_to_rented_books(self, name, book, days_to_return):
+    def __add_to_rented_books(self, name, book, days_to_return):
         self.rented_books[name] = self.rented_books.get(name, {})
         self.rented_books[name].update({book: days_to_return})
 
-    def remove_from_rented_books(self, name, book):
+    def __remove_from_rented_books(self, name, book):
         del self.rented_books[name][book]
 
     @staticmethod
-    def add_to_user_books(user, book):
+    def __add_to_user_books(user, book):
         user.books.append(book)
 
     @staticmethod
-    def remove_from_user_books(user, book):
+    def __remove_from_user_books(user, book):
         user.books.remove(book)
--- softuni/Python OOP/Classes and Objects/Excersise/project/registration.py
+++ softuni/Python OOP/Classes and Objects/Excersise/project/registration.py
@@ -4,43 +4,43 @@ from .library import Library
 
 class Registration:
     def add_user(self, user: User, library: Library):
-        if self.contains_user(user.user_id, library.user_records):
+        if self.__contains_user(user.user_id, library.user_records):
             return f"User with id = {user.user_id} already registered in the library!"
 
-        return self.add_to_user_records(library.user_records,user)
+        return self.__add_to_user_records(library.user_records,user)
 
     def remove_user(self, user: User, library: Library):
-        if not self.contains_user(user.user_id, library.user_records):
+        if not self.__contains_user(user.user_id, library.user_records):
             return "We could not find such user to remove!"
 
-        return self.remove_from_user_records(library.user_records, user)
+        return self.__remove_from_user_records(library.user_records, user)
 
     def change_username(self, _id: int, new_username, library: Library):
-        user_to_change = self.contains_user(_id, library.user_records)
+        user_to_change = self.__contains_user(_id, library.user_records)
         if not user_to_change:
             return f'There is no user with id = {_id}!'
         if user_to_change.username == new_username:
             return "Please check again the provided username - it should be different than the username used so far!"
 
-        return self.change_name(user_to_change, new_username, _id, library)
+        return self.__change_name(user_to_change, new_username, _id, library)
 
     @staticmethod
-    def contains_user(user: int, library):
+    def __contains_user(user: int, library):
         for x in library:
             if x.user_id == user:
                 return x
         return None
 
     @staticmethod
-    def add_to_user_records(records, user: User):
+    def __add_to_user_records(records, user: User):
         records.append(user)
 
     @staticmethod
-    def remove_from_user_records(records, user: User):
+    def __remove_from_user_records(records, user: User):
         records.remove(user)
 
     @staticmethod
-    def change_name(old, new, _id, library):
+    def __change_name(old, new, _id, library):
         if old.username in library.rented_books.keys():
             library.rented_books[new] = library.rented_books.pop(old.username)
         old.username = new
